2fc017d4338d629eeded66d71e0e835f8227da68
Mayank Jain <10.mayank.jain@gmail.com>
2025-02-05 22:59:32 +0530

    HashMap start

--- src/collections/engineeringDigest/Map/HashMapMain.java
+++ src/collections/engineeringDigest/Map/HashMapMain.java
@@ -1,7 +1,45 @@
 package collections.engineeringDigest.Map;
 
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
 public class HashMapMain {
 	public static void main(String[] args) {
-		
+
+		// order of elements is not maintained here
+		HashMap<Integer, String> map = new HashMap<>();
+		map.put(5, "Annie"); // O(1)
+		map.put(1, "Alice");
+		map.put(2, "Bob");
+		map.put(3, "Charlie");
+		map.put(4, "Dhruv");
+		System.out.println(map);
+
+		System.out.println(map.get(3)); // O(1)
+
+		System.out.println(map.get(45));
+
+		System.out.println(map.containsKey(2)); // O(1)
+
+		System.out.println(map.containsValue("Bob"));
+
+		for (Integer key : map.keySet()) {
+			System.out.println(key + " -> " + map.get(key));
+		}
+
+		for (Map.Entry<Integer, String> entry : map.entrySet()) {
+			System.out.println(entry.getKey() + " -> " + entry.getValue());
+			entry.setValue(entry.getValue().toUpperCase());
+		}
+		System.out.println(map);
+
+		map.remove(2);
+		// map.remove();
+		System.out.println(map);
+		System.out.println(map.remove(3, "Alice"));
+		System.out.println(map);
+
+
 	}
 }
